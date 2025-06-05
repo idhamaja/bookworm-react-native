f0d9a3ca52df10e896a26a6e054abbc84898dbd4
Idham Ganteng <idham16iramap.com>
2025-06-05 13:56:51 +0700

    update cron job file

--- index.js
+++ index.js
@@ -4,21 +4,26 @@ import authRoutes from "./src/routes/authRoutes.js";
 import bookRoutes from "./src/routes/bookRoutes.js";
 import { connectDB } from "./src/lib/db.js";
 import cors from "cors";
+import job from "./src/lib/cron.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+job.start();
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
 // Connect to DB first
-connectDB().then(() => {
-  console.log("Database connected");
-}).catch(err => {
-  console.error("Database connection failed", err);
-  process.exit(1);
-});
+connectDB()
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.error("Database connection failed", err);
+    process.exit(1);
+  });
 
 // Routes
 app.use("/api/auth", authRoutes);
@@ -27,9 +32,12 @@ app.use("/api/books", bookRoutes);
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ 
+  res.status(500).json({
     success: false,
-    error: process.env.NODE_ENV === 'development' ? err.message : 'Internal server error'
+    error:
+      process.env.NODE_ENV === "development"
+        ? err.message
+        : "Internal server error",
   });
 });
 
